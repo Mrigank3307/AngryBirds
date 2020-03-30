c0c2b1e700d130eb2d87631ec3ca6571ae9a7c66
Mrigank3307 <Mrigank030307@outlook.com>
2020-03-30 18:36:59 +0530

    added a platform

--- sketch.js
+++ sketch.js
@@ -35,6 +35,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    platform = new Ground (150,305,300,170);
+
 }
 
 function draw(){
@@ -57,6 +59,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+ platform.display();
 
     bird.display();
 }
